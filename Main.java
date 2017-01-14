654ea0e55adbc1af3b9e06bb9e9320967677d3e8
phantomhives <shusanta_ss@yahoo.com>
2017-01-14 18:58:53 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("HelloWorld");
+
+        int Number1 = 10;
+        int Number2 = 25;
+
+        int Sum = Number1+Number2;
+
+        System.out.println("Total sum is:"+Sum);
+    }
+}
