3ea42d531b991f33f9410cf627656bed36f8cb39
phantomhives <shusanta_ss@yahoo.com>
2017-01-25 16:34:01 +0530

    Add files via upload

---
+++ java6.java
@@ -0,0 +1,83 @@
+package com.company;
+
+/**
+ * Created by shusa on 19-Jan-17.
+ */
+
+import com.sun.org.apache.bcel.internal.classfile.SourceFile;
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+
+public class java6 {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+//        for (int i=1; i<=4; i++)
+//        {
+//            System.out.printf("%2d", i);
+//        }
+//
+//
+
+//        int i;
+//        int j;
+//        int k;
+//
+//        System.out.println("enter a number: ");
+//        i =input.nextInt();
+//
+//        System.out.println("enter another number:");
+//        j =input.nextInt();
+//
+//        System.out.println("Enter another number: ");
+//        k =input.nextInt();
+//
+//        if (i>j)
+//        {
+//            System.out.println(i);                     //2.17
+//
+//            if (j>k)
+//            {
+//                System.out.println(j);
+//            }
+//        }else
+//        {
+//            System.out.println(k);
+//        }
+
+
+//        System.out.printf("%s%n%s%n%s%n", "*", "***", "*****");
+//        System.out.printf("  *%n ***%n*****%n  *%n  *%n  *%n  *%n  *%n  *%n");   This is arow
+//        System.out.printf("*********%n*       *%n*       *%n*       *%n*       *%n*       *%n*       *%n*       *" +
+//                "%n*********");n                                              This is box
+//        System.out.printf("   ***%n *     *%n*       *%n*       *%n*       *%n*       *%n *     *%n   ***"); Oval
+
+
+//        int startNumber;
+//
+//        while(startNumber<11)
+//        {
+//            System.out.printf("7 x %02d = %2d%n",startNumber,(7*startNumber));
+//            startNumber++;
+//        }
+//
+//        for (int i= 1; i<11 ; i++)
+//        {
+//            System.out.printf("15 x %02d = %3d%n",i,(i*15));
+//        }
+
+//        System.out.print("please enter a number:");
+//        int number =input.nextInt();
+//
+//        for (int i= 1; i<11 ; i++)
+//        {
+//            System.out.printf("%d x %02d = %3d%n",number,i,(number*i));
+//        }
+
+
+
+    }
+}
---
+++ java7.java
@@ -0,0 +1,112 @@
+package com.company;
+
+/**
+ * Created by shusa on 22-Jan-17.
+ */
+import java.util.Scanner;
+
+public class java7 {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[]args) {
+
+//
+        //       System.out.print("Enter your mark: ");
+//        double gradeValue =input.nextDouble();
+//
+//        if (gradeValue <0 || gradeValue >100)
+//        {
+//            System.out.println("Wrong input");
+//        }else if (gradeValue >=0 && gradeValue <=33)
+//        {
+//            System.out.println("F");
+//        }else if (gradeValue >33 && gradeValue <40)
+//        {
+//            System.out.println("D");
+//        }else if (gradeValue >39 && gradeValue <50)
+//        {
+//            System.out.println("C");
+//        }else if (gradeValue >49 && gradeValue <60)
+//        {
+//            System.out.println("B");
+//        }else if (gradeValue >59 && gradeValue <70)
+//        {
+//            System.out.println("A-");
+//        }else if (gradeValue >69 && gradeValue <80)
+//        {
+//            System.out.println("A");
+//        }else
+//        {
+//            System.out.println("A+");
+//        }
+
+
+//        int number;
+//        int largest =0;
+//        int smallest =-0;
+//
+//        for (int i = 1; i <=5; i++)
+//        {
+//            System.out.print("Enter a number: ");
+//            number = input.nextInt();
+//            if (i==1)
+//            {
+//                largest = number;
+//                smallest =number;
+//            }else
+//            {
+//                if (number>largest)
+//                {
+//                    largest = number;
+//                }
+//                if (number<smallest)
+//                {
+//                    smallest = number;
+//                }
+//            }
+//        }
+//        System.out.println("largest number: " + largest);
+//        System.out.println("smallest number: " + smallest);
+
+
+//        int number =42339;
+//        int reminder = 0;
+
+//        for (int i =4; i>=0 ;i--)
+//        {
+//            System.out.print((int)(number/Math.pow(10,i)) + "   ");
+//            number =(int)(number%Math.pow(10,i));
+//        }
+//
+//        for (int i =1; i<=8 ;i++)
+//        {
+//            if (i%2==0)
+//            {
+//                System.out.println(" * * * * * * * *");
+//            }else
+//            {
+//                System.out.println("* * * * * * * *");
+//            }
+//        }
+
+
+//        int number;
+//        System.out.print("enter a number: ");
+//        number = input.nextInt();
+//
+//        if (number % 2 == 0) {
+//            System.out.println("even");
+//        } else
+//        {
+//            System.out.println("odd");
+//        }
+
+
+
+
+
+
+
+    }
+}
